2afda778c31b72e9faff8771a5bf5d58963ecc17
VicMeGa <vicmega2004@gmail.com>
2025-06-15 20:38:23 -0600

    correciones

--- src/main/java/com/vantus/project/WebConfig.java
+++ src/main/java/com/vantus/project/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Puedes restringir por ruta si deseas
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("*")
                         .allowedMethods("*") // GET, POST, PUT, DELETE, etc.
                         .allowedHeaders("*");
             }
